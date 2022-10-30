5bf8b202b02fdbc9ea27c233fe8cbf4b3ddb94dd
RonaldoWidiantoSiswoyo <ronaldowidianto123@gmail.com>
2022-10-30 23:12:29 +0700

    footer baru

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
+    <div className={styles.footer} id="contact">
       <div className={styles["footer__container"]}>
         <div className={styles["footer__first"]}>
           <div className={styles["footer__location"]}>
